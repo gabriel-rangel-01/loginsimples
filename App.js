9535403173c3f5fea506ff2d80c23344804983d7
rangel <gabriel.rangel.oliveira@escola.pr.gov.br>
2025-07-09 09:19:49 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,61 +1,31 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import { View, Text, TextInput, Pressable, StyleSheet } from 'react-native';
+import { NavigationConteiner } from '@react-navigation/native';
 
+import { CreateStackNavigator } from '@react-navigation/stack';
 
-const loginScreen = ({ navegation }) => {
-  const [email, setemail] = useState('');
-  const [password, setPassword] = useState('');
+import HomeScreen from './Screen/HomeScreen';
 
-const handLelogin = () => {
+import SeconderyScreen from './Screen/SecondaryScreen';
 
-  if (true) {
-    navigation.navegate('HomeScreen');
-  }else {
-    alert('por favor, preencha todos os campos.');
-  }
-};
+import CafeScreen from './Screen/CafeScreen';
 
-return (
-  <View style={styles.conteiner}>
-  <TextInput style={styles.input} placeholder='E-mail' value={email} onChangeText={setEmail}/>
-  <TextInput style={styls.input} placeholder='senha' secureTextEntry={true} value={password} onChangeText={setPassword}/>
-  <Pressable style={styles.buttons} onPress={handLelogin}>
-  <Text style= {styles.buttonText}>Login</Text></Pressable>
- </View>
+import BoloScreen from './Screen/BoloScreen';
+
+import PaoScreen from './Screen/PaoScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+ return (
+   <NavigationConteiner>
+     <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen Name="Home" Component= {HomeScreen} Options={{title: 'Bem-Vindo!'}} />
+      <Stack.Screen Name="Secondary" Component= {SeconderyScreen} Options={{title: 'Opções Deliciosas'}} />
+      <Stack.Screen Name="Cafe" Component= {CafeScreen} Options={{title: 'Nosso Café'}} />
+      <Stack.Screen Name="Bolo" Component= {BoloScreen} Options={{title: 'Bolo de Chocolate'}} />
+      <Stack.Screen Name="Pao" Component= {PaoScreen} Options={{title: 'Pão Caseiro'}} />
+     </Stack.Navigator>
+   </NavigationConteiner>
  );
-};
-
-const styles = StyleSheet.create({
-  conteiner:{
-    backgroundColor: '#6A0C0B'
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-    padding: 20,
-    color white,
-  },
-  input: {
-    width: '100%',
-    height: 40,
-    borderColor: '#FBCA03',
-    color: 'white',
-    borderwidth: 1,
-    paddingHorizontal: 10,
-    marginBottom: 10,
-  },
-  button: {
-    backgroundColor: '#AA0505',
-    padding: 10,
-    borderRadius: 5,
-    corderColor: '#FBCA03',
-  },
-  buttonText: {
-    color: 'white',
-    textAlign: 'center',
-  },
-});
-export default loginScreen;
-
-
-
+}
